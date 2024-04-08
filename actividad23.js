b980adab77f0342291bd175fd48bece4d8bb981c
Erubiel Cuevas <158236707+Cheffsiito@users.noreply.github.com>
2024-04-08 15:57:00 -0700

    Add files via upload

---
+++ actividad23.js
@@ -0,0 +1,12 @@
+// Daniel Jimenez Manrique
+function numeroParImpar() {
+    var numero = 0;
+    numero = parseInt(prompt("Ingrese el numero"));
+    
+    if((numero % 2) == 0){
+        alert(numero + " es par");
+    }else{
+      alert(numero + " es impar");
+    } 
+}    
+
---
+++ actividad24.js
@@ -0,0 +1,13 @@
+// Daniel Jimenez Manrique
+var hayUnHuracan = true
+function Aviso_De_Huracam(hayUnHuracan) {
+    
+
+    if (hayUnHuracan) {
+        alert("Hay un Huracan");
+    }else{
+        alert("Esta Soleado");
+
+    }
+    
+}
